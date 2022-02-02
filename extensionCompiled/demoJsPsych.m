d2d8818f324c43d3d4dd785688ac61ec8e55c8c3
Pedro Neto <pdealcan@jyu-mac-20210915.local>
2022-02-02 14:02:09 +0200

    matlab functions

--- extensionCompiled/demoJsPsych.m
+++ extensionCompiled/demoJsPsych.m
@@ -2,12 +2,16 @@ cd '/Users/pdealcan/Documents/github/accelWeb/extensionCompiled/'
 addpath('/Users/pdealcan/Documents/github/matlabTools/MocapToolbox/mocaptoolbox')
 a = readJsPsych('./mydata.json', 2);
 
-%Basic information about data collection. Before interpolation
-a.other
+%MoCap data structure
+a
+
+%Interpolated data
 a.data
 
-%%%Use MoCap functions
+%Basic information about data collection. Before interpolation
+a.other
 
+%%%Use MoCap functions
 %Visualizing
 mcplottimeseries(a, 1, 'dim', 1) %x
 mcplottimeseries(a, 1, 'dim', 2) %y
@@ -20,12 +24,3 @@ mcplottimeseries(jsd, 1, 'dim', 3)
 %Period analysis
 [per, ac, eac, lags, wstart] = mcwindow(@mcperiod, jsd, 2, 0.25);
 plot(wstart,per(:,3))
-
-imagesc(eac(:,:,3)), axis xy
-     set(gcf,'Position',[40 200 560 420])
-     set(gca,'XTick',0:2:32)
-     set(gca,'XTickLabel',0.5*(0:2:32))
-     set(gca,'YTick',[1 51 101 151 201])
-     set(gca,'YTickLabel',[0 0.5 1 1.5 2.0])
-     xlabel('Time / secs')
-     ylabel('Period /secs')
