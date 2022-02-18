206188906bb9ab6fdcf93bd80bd1b35e41f85515
Pedro Neto <pdealcan@jyu-mac-20210915.local>
2022-02-18 10:09:18 +0200

    labels

--- jsPsychExtension/jspsych/dist/graphFunction.js
+++ jsPsychExtension/jspsych/dist/graphFunction.js
@@ -1,27 +1,31 @@
 //Generates data object to feed into graph
-function dataGenerator(dataSet){
+function dataGenerator(yAxis, labels){
   var dataConstructor = [];
-  for (var i=0; i<dataSet.length; i++) {
+  for (var i=0; i<yAxis.length; i++) {
       var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+      var label = labels[i]
       dataConstructor[i] = {
-          data: dataSet[i],
+          label: label,
+          data: yAxis[i],
           borderColor: randomColor,
           fill: false 
       };
   }
+  console.log(dataConstructor)
   return(dataConstructor)
 }
 
 //Generates graph and appends to given element by ID
-function graphCustom(dataSet, id, type, xLabels){
-  var dataConstructor = dataGenerator(dataSet)
+function graphCustom(xAxis, yAxis, labels, id, type){
+  var dataConstructor = dataGenerator(yAxis, labels)
   new Chart(id, {
     type: type,
     data: {
-      labels: xLabels,
+      labels: xAxis,
       datasets: dataConstructor },
     options: {
-      legend: {display: false}
+      legend: {display: true}
     }
   });
 };
+
