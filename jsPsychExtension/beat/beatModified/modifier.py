cc92f8075828fc463efd1ee693d78e888e7cd3c7
Pedro Neto <pasoneto@gmail.com>
2022-08-19 10:03:00 +0300

    song

--- jsPsychExtension/beat/beatModified/modifier.py
+++ jsPsychExtension/beat/beatModified/modifier.py
@@ -2,13 +2,16 @@ import numpy as np
 import soundfile
 from pydub import AudioSegment
 
-def createBeat(bpm, shiftN, beat, name):
-    oneBeat = (1/bpm)*60000
-    eighth = oneBeat/8
-    silentDurationOneBeat = eighth - len(beat)
+def createBeat(segundosSegment, nBeatsSegment, shiftN, beat, name, repetitions):
+    
+    segundosSegment = segundosSegment*1000
+    segundosSegment = segundosSegment/nBeatsSegment
+    segundosEight = segundosSegment/8
+
+    silentDurationOneBeat = segundosEight - len(beat)
 
     silent = AudioSegment.silent(duration=silentDurationOneBeat)
-    silentEighth = AudioSegment.silent(duration=eighth)
+    silentEighth = AudioSegment.silent(duration=segundosEight)
     soundBeatEighth = beat + silent
 
     beatPosition = [1, 0, 0, 0, 0, 0, 0, 0]
@@ -21,25 +24,33 @@ def createBeat(bpm, shiftN, beat, name):
         else:
             mergeBeat = mergeBeat+silentEighth
 
-    soundOneBar = mergeBeat+mergeBeat+mergeBeat+mergeBeat
-
-    print(len(soundOneBar))
-
     bar = AudioSegment.empty()
-    nBars = 10
-    for i in range(nBars):
-        bar = bar + soundOneBar
-
+    for i in range(repetitions):
+        bar = bar + mergeBeat
+        
     bar.export(f"./{name}_{shiftN}.wav", format="wav")
 
 beat = AudioSegment.from_wav("./beatUnit.wav")
-bpm = 75.1526
 
-createBeat(bpm, 0, beat, "metronome")
-createBeat(bpm, 1, beat, "metronome")
-createBeat(bpm, 2, beat, "metronome")
-createBeat(bpm, 3, beat, "metronome")
-createBeat(bpm, 4, beat, "metronome")
-createBeat(bpm, 5, beat, "metronome")
-createBeat(bpm, 6, beat, "metronome")
-createBeat(bpm, 7, beat, "metronome")
+sr = 24 #Sampling rate used to measure nSamples of excerpt
+beats = 8 #Number of beats within excerpt
+frames = 154 #Number of frames within excerpt
+
+segundosTotal = frames/sr
+segundosBeat = segundosTotal/beats
+
+segundosSegment = segundosBeat*beats #How many seconds does the whole segment have
+nBeatsSegment = 8 #How many beats happen throughout all of the segment
+repetitions = nBeatsSegment*1 #How many repetitions for each segment should the output have
+
+#The output of this function will not be exactly the same as the length of the excerpt used as model.
+#Excerpt should then be trimmed to fit perfectly with output of this function
+#Then I come back here and set the number of repertitions to cover how many segments I want for each trial
+createBeat(segundosSegment, nBeatsSegment, 0, beat, "metronome", repetitions)
+createBeat(segundosSegment, nBeatsSegment, 1, beat, "metronome", repetitions)
+createBeat(segundosSegment, nBeatsSegment, 2, beat, "metronome", repetitions)
+createBeat(segundosSegment, nBeatsSegment, 3, beat, "metronome", repetitions)
+createBeat(segundosSegment, nBeatsSegment, 4, beat, "metronome", repetitions)
+createBeat(segundosSegment, nBeatsSegment, 5, beat, "metronome", repetitions)
+createBeat(segundosSegment, nBeatsSegment, 6, beat, "metronome", repetitions)
+createBeat(segundosSegment, nBeatsSegment, 7, beat, "metronome", repetitions)
