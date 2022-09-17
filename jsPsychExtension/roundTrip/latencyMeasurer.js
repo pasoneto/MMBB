26ed143cc4218c2772548f91e5d56b9d1a834fe8
Pedro Neto <pasoneto@gmail.com>
2022-09-17 12:49:44 +0300

    mic console log

--- jsPsychExtension/roundTrip/latencyMeasurer.js
+++ jsPsychExtension/roundTrip/latencyMeasurer.js
@@ -21,22 +21,3 @@ async function startStop(nLoops){
   }
   oscillator.stop()
 }
-
-function getLocalStream() {
-    navigator.mediaDevices.getUserMedia({video: false, audio: true}).then((stream) => {
-        window.localStream = stream;
-        window.localAudio.srcObject = stream;
-        window.localAudio.autoplay = true;
-        const mediaRecorder = new MediaRecorder(stream);
-        mediaRecorder.start(); 
-
-        mediaRecorder.addEventListener("dataavailable", event => {
-          console.log(event.data);
-        });
-
-    }).catch((err) => {
-        console.error(`you got an error: ${err}`)
-    });
-}
-
-getLocalStream();
