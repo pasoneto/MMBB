9be2467b6e045ef0c00bac02f2640b3eca53a31a
Pedro Neto <pasoneto@gmail.com>
2022-10-05 11:55:55 +0300

    a

--- songs/movementTapAudio/songKeys.js
+++ songs/movementTapAudio/songKeys.js
@@ -2,24 +2,17 @@ const path = require('path');
 const fs = require('fs');
 var util = require('util');
 
-var directoryPath = "/Users/pdealcan/Documents/github/accelWeb/jsPsychExtension/songs/movementTapAudio/modifiedAudio";
-
 var directoryPath = "/Users/pdealcan/Documents/github/accelWeb/songs/movementTapAudio/modifiedAudio"
 
 var songKeys = {
   "1": [],
   "2": [],
+  "3": [],
   "4": [],
   "5": [],
   "6": [], 
   "7": [], 
   "8": [], 
-  "9": [],
-  "10": [],
-  "11": [], 
-  "12": [],
-  "13": [],
-  "14": []
 } 
 
 var files = fs.readdirSync(directoryPath);
@@ -36,5 +29,3 @@ for(k in objectKeys){
 }
 
 fs.writeFileSync('./songKeys.json', JSON.stringify(songKeys), 'utf-8');
-
-
--- songs/movementTapAudio/songKeysNames.js
+++ songs/movementTapAudio/songKeysNames.js
@@ -15,85 +15,52 @@ var songKeys = {
         "name__2 - Seeb - Simple Life__stretchfactor__1__0_75188__1_25.mp3",
         "name__2 - Seeb - Simple Life__stretchfactor__1__1_25__0_75188.mp3"
     ],
+    "3": [
+        "name__3 - Beats Antique - Discovered__stretchfactor__0_7218__0_96__1_2.mp3",
+        "name__3 - Beats Antique - Discovered__stretchfactor__0_7218__1_2__0_96.mp3",
+        "name__3 - Beats Antique - Discovered__stretchfactor__0_96__0_7218__1_2.mp3",
+        "name__3 - Beats Antique - Discovered__stretchfactor__0_96__1_2__0_7218.mp3",
+        "name__3 - Beats Antique - Discovered__stretchfactor__1_2__0_7218__0_96.mp3",
+        "name__3 - Beats Antique - Discovered__stretchfactor__1_2__0_96__0_7218.mp3"
+    ],
     "4": [
-        "name__4 - F-777 - Airborne Robots__stretchfactor__0_75188__1_25__1.mp3",
-        "name__4 - F-777 - Airborne Robots__stretchfactor__0_75188__1__1_25.mp3",
-        "name__4 - F-777 - Airborne Robots__stretchfactor__1_25__0_75188__1.mp3",
-        "name__4 - F-777 - Airborne Robots__stretchfactor__1_25__1__0_75188.mp3",
-        "name__4 - F-777 - Airborne Robots__stretchfactor__1__0_75188__1_25.mp3",
-        "name__4 - F-777 - Airborne Robots__stretchfactor__1__1_25__0_75188.mp3"
+        "name__4 - Hypnotic Brass Ensemble - Rebel Rousin__stretchfactor__0_77444__1_03__1_2875.mp3",
+        "name__4 - Hypnotic Brass Ensemble - Rebel Rousin__stretchfactor__0_77444__1_2875__1_03.mp3",
+        "name__4 - Hypnotic Brass Ensemble - Rebel Rousin__stretchfactor__1_03__0_77444__1_2875.mp3",
+        "name__4 - Hypnotic Brass Ensemble - Rebel Rousin__stretchfactor__1_03__1_2875__0_77444.mp3",
+        "name__4 - Hypnotic Brass Ensemble - Rebel Rousin__stretchfactor__1_2875__0_77444__1_03.mp3",
+        "name__4 - Hypnotic Brass Ensemble - Rebel Rousin__stretchfactor__1_2875__1_03__0_77444.mp3"
     ],
     "5": [
-        "name__5 - Rezz - Edge__stretchfactor__0_75188__1_25__1.mp3",
-        "name__5 - Rezz - Edge__stretchfactor__0_75188__1__1_25.mp3",
-        "name__5 - Rezz - Edge__stretchfactor__1_25__0_75188__1.mp3",
-        "name__5 - Rezz - Edge__stretchfactor__1_25__1__0_75188.mp3",
-        "name__5 - Rezz - Edge__stretchfactor__1__0_75188__1_25.mp3",
-        "name__5 - Rezz - Edge__stretchfactor__1__1_25__0_75188.mp3"
+        "name__5 - Calvin Harris - Slow Acid__stretchfactor__0_78195__1_04__1_3.mp3",
+        "name__5 - Calvin Harris - Slow Acid__stretchfactor__0_78195__1_3__1_04.mp3",
+        "name__5 - Calvin Harris - Slow Acid__stretchfactor__1_04__0_78195__1_3.mp3",
+        "name__5 - Calvin Harris - Slow Acid__stretchfactor__1_04__1_3__0_78195.mp3",
+        "name__5 - Calvin Harris - Slow Acid__stretchfactor__1_3__0_78195__1_04.mp3",
+        "name__5 - Calvin Harris - Slow Acid__stretchfactor__1_3__1_04__0_78195.mp3"
     ],
     "6": [
-        "name__6 - Beats Antique - Discovered__stretchfactor__0_7218__0_96__1_2.mp3",
-        "name__6 - Beats Antique - Discovered__stretchfactor__0_7218__1_2__0_96.mp3",
-        "name__6 - Beats Antique - Discovered__stretchfactor__0_96__0_7218__1_2.mp3",
-        "name__6 - Beats Antique - Discovered__stretchfactor__0_96__1_2__0_7218.mp3",
-        "name__6 - Beats Antique - Discovered__stretchfactor__1_2__0_7218__0_96.mp3",
-        "name__6 - Beats Antique - Discovered__stretchfactor__1_2__0_96__0_7218.mp3"
+        "name__6 - Emad Sayyah - Dala' (Temptation)__stretchfactor__0_74436__0_99__1_2375.wav",
+        "name__6 - Emad Sayyah - Dala' (Temptation)__stretchfactor__0_74436__1_2375__0_99.wav",
+        "name__6 - Emad Sayyah - Dala' (Temptation)__stretchfactor__0_99__0_74436__1_2375.wav",
+        "name__6 - Emad Sayyah - Dala' (Temptation)__stretchfactor__0_99__1_2375__0_74436.wav",
+        "name__6 - Emad Sayyah - Dala' (Temptation)__stretchfactor__1_2375__0_74436__0_99.wav",
+        "name__6 - Emad Sayyah - Dala' (Temptation)__stretchfactor__1_2375__0_99__0_74436.wav"
     ],
     "7": [
-        "name__7 - Johannes Linstead - Cafe Tropical__stretchfactor__0_76692__1_02__1_275.mp3",
-        "name__7 - Johannes Linstead - Cafe Tropical__stretchfactor__0_76692__1_275__1_02.mp3",
-        "name__7 - Johannes Linstead - Cafe Tropical__stretchfactor__1_02__0_76692__1_275.mp3",
-        "name__7 - Johannes Linstead - Cafe Tropical__stretchfactor__1_02__1_275__0_76692.mp3",
-        "name__7 - Johannes Linstead - Cafe Tropical__stretchfactor__1_275__0_76692__1_02.mp3",
-        "name__7 - Johannes Linstead - Cafe Tropical__stretchfactor__1_275__1_02__0_76692.mp3"
+        "name__7 - El Pesebre - Afrosound__stretchfactor__0_72932__0_97__1_2125.mp3",
+        "name__7 - El Pesebre - Afrosound__stretchfactor__0_72932__1_2125__0_97.mp3",
+        "name__7 - El Pesebre - Afrosound__stretchfactor__0_97__0_72932__1_2125.mp3",
+        "name__7 - El Pesebre - Afrosound__stretchfactor__0_97__1_2125__0_72932.mp3",
+        "name__7 - El Pesebre - Afrosound__stretchfactor__1_2125__0_72932__0_97.mp3",
+        "name__7 - El Pesebre - Afrosound__stretchfactor__1_2125__0_97__0_72932.mp3"
     ],
     "8": [
-        "name__8 - Hypnotic Brass Ensemble - Rebel Rousin__stretchfactor__0_77444__1_03__1_2875.mp3",
-        "name__8 - Hypnotic Brass Ensemble - Rebel Rousin__stretchfactor__0_77444__1_2875__1_03.mp3",
-        "name__8 - Hypnotic Brass Ensemble - Rebel Rousin__stretchfactor__1_03__0_77444__1_2875.mp3",
-        "name__8 - Hypnotic Brass Ensemble - Rebel Rousin__stretchfactor__1_03__1_2875__0_77444.mp3",
-        "name__8 - Hypnotic Brass Ensemble - Rebel Rousin__stretchfactor__1_2875__0_77444__1_03.mp3",
-        "name__8 - Hypnotic Brass Ensemble - Rebel Rousin__stretchfactor__1_2875__1_03__0_77444.mp3"
-    ],
-    "9": [
-        "name__9 - Skogsra - Primadonna__stretchfactor__0_73684__0_98__1_225.mp3",
-        "name__9 - Skogsra - Primadonna__stretchfactor__0_73684__1_225__0_98.mp3",
-        "name__9 - Skogsra - Primadonna__stretchfactor__0_98__0_73684__1_225.mp3",
-        "name__9 - Skogsra - Primadonna__stretchfactor__0_98__1_225__0_73684.mp3",
-        "name__9 - Skogsra - Primadonna__stretchfactor__1_225__0_73684__0_98.mp3",
-        "name__9 - Skogsra - Primadonna__stretchfactor__1_225__0_98__0_73684.mp3"
-    ],
-    "10": [
-        "name__10 - Bonobo - Ketto__stretchfactor__0_71429__0_95__1_1875.mp3",
-        "name__10 - Bonobo - Ketto__stretchfactor__0_71429__1_1875__0_95.mp3",
-        "name__10 - Bonobo - Ketto__stretchfactor__0_95__0_71429__1_1875.mp3",
-        "name__10 - Bonobo - Ketto__stretchfactor__0_95__1_1875__0_71429.mp3",
-        "name__10 - Bonobo - Ketto__stretchfactor__1_1875__0_71429__0_95.mp3",
-        "name__10 - Bonobo - Ketto__stretchfactor__1_1875__0_95__0_71429.mp3"
-    ],
-    "11": [],
-    "12": [
-        "name__12 - Emad Sayyah - Dala' (Temptation)__stretchfactor__0_74436__0_99__1_2375.wav",
-        "name__12 - Emad Sayyah - Dala' (Temptation)__stretchfactor__0_74436__1_2375__0_99.wav",
-        "name__12 - Emad Sayyah - Dala' (Temptation)__stretchfactor__0_99__0_74436__1_2375.wav",
-        "name__12 - Emad Sayyah - Dala' (Temptation)__stretchfactor__0_99__1_2375__0_74436.wav",
-        "name__12 - Emad Sayyah - Dala' (Temptation)__stretchfactor__1_2375__0_74436__0_99.wav",
-        "name__12 - Emad Sayyah - Dala' (Temptation)__stretchfactor__1_2375__0_99__0_74436.wav"
-    ],
-    "13": [
-        "name__13 - El Pesebre - Afrosound__stretchfactor__0_72932__0_97__1_2125.mp3",
-        "name__13 - El Pesebre - Afrosound__stretchfactor__0_72932__1_2125__0_97.mp3",
-        "name__13 - El Pesebre - Afrosound__stretchfactor__0_97__0_72932__1_2125.mp3",
-        "name__13 - El Pesebre - Afrosound__stretchfactor__0_97__1_2125__0_72932.mp3",
-        "name__13 - El Pesebre - Afrosound__stretchfactor__1_2125__0_72932__0_97.mp3",
-        "name__13 - El Pesebre - Afrosound__stretchfactor__1_2125__0_97__0_72932.mp3"
-    ],
-    "14": [
-        "name__14 - metronome_agogo_hemiola__stretchfactor__0_75188__1_25__1.mp3",
-        "name__14 - metronome_agogo_hemiola__stretchfactor__0_75188__1__1_25.mp3",
-        "name__14 - metronome_agogo_hemiola__stretchfactor__1_25__0_75188__1.mp3",
-        "name__14 - metronome_agogo_hemiola__stretchfactor__1_25__1__0_75188.mp3",
-        "name__14 - metronome_agogo_hemiola__stretchfactor__1__0_75188__1_25.mp3",
-        "name__14 - metronome_agogo_hemiola__stretchfactor__1__1_25__0_75188.mp3"
+        "name__8 - metronome_agogo_hemiola__stretchfactor__0_75188__1_25__1.mp3",
+        "name__8 - metronome_agogo_hemiola__stretchfactor__0_75188__1__1_25.mp3",
+        "name__8 - metronome_agogo_hemiola__stretchfactor__1_25__0_75188__1.mp3",
+        "name__8 - metronome_agogo_hemiola__stretchfactor__1_25__1__0_75188.mp3",
+        "name__8 - metronome_agogo_hemiola__stretchfactor__1__0_75188__1_25.mp3",
+        "name__8 - metronome_agogo_hemiola__stretchfactor__1__1_25__0_75188.mp3"
     ]
 }
