37c4f5d7629c5d012deb9955c23a44315a0b369c
Pedro Neto <pasoneto@gmail.com>
2022-10-10 19:10:33 +0300

    input test

---
+++ src/batteries/pitchDetect.js
@@ -0,0 +1,11 @@
+class PitchDetect extends AudioWorkletProcessor {
+  constructor() {
+    super();
+  }
+  process (inputs, outputs, parameters) {
+    console.log(inputs);
+    console.log(outouts);
+  }
+}
+
+registerProcessor('pitchDetect', Player)
