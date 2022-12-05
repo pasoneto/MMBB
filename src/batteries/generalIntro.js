361d367e08e8fbad247cd6ed56f77a33c2012311
Pedro Neto <pasoneto@gmail.com>
2022-12-05 12:04:11 +0200

    test accel

--- src/batteries/generalIntro.js
+++ src/batteries/generalIntro.js
@@ -30,6 +30,16 @@ var gettingHelp = {
   },
 };
 
+var lockScreen = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: initialInstructions[0][3][lang],
+  choices: [initialInstructions[0][2][lang], initialInstructions[0][1][lang]],
+  prompt: '',
+  on_load: function(){
+    document.getElementById("jspsych-html-button-response-btngroup").getElementsByClassName("jspsych-btn")[1].style.background = "purple"
+  },
+};
+
 var howDifficult = {
     type: jsPsychHtmlSliderResponse,
     stimulus: recurring[9][lang],
