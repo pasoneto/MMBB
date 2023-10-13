47750c5a9689695a82882ccf0acdd89a533a051f
Pedro Neto <pasoneto@gmail.com>
2023-10-13 12:33:06 +0300

    shared measurements studyID

--- utils/studyIDs.js
+++ utils/studyIDs.js
@@ -1,5 +1,7 @@
 var studyIDtasks = {
-    "main": ["Movement", "Survey", "Singing", "Emotion", "Rhythm", "MBEMA", "SharedMeasures"],
-    "123":  ["Emotion", "Survey"],
+    "main": ["Movement", "Singing", "Emotion", "Rhythm", "MBEMA", "SharedMeasures"],
+    "emotion":  ["Emotion", "SharedMeasurements"],
+    "accelClass":  ["Movement", "SharedMeasurements"],
+    "movementWhole":  ["Movement", "Rhythm", "SharedMeasurements"],
     "alicia": ["Rhythm"]
   }
