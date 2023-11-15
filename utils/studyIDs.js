4e11f33f4227bf3517fbf1e0c5db9db6ff13b7ff
Pedro Neto <pasoneto@gmail.com>
2023-11-15 14:50:03 +0200

    buttons

--- utils/studyIDs.js
+++ utils/studyIDs.js
@@ -2,7 +2,7 @@ var studyIDtasks = {
     "main": ["Movement", "Singing", "Emotion", "Rhythm", "MBEMA", "SharedMeasures"],
     "emotion":  ["Emotion", "SharedMeasures"],
     "emotionAdaptive":  ["EmotionAdaptive", "SharedMeasures"],
-    "accelClass":  ["Movement", "SharedMeasures"],
+    "accelClass":  ["Movement"],
     "movementWhole":  ["Movement", "Rhythm", "SharedMeasures"],
     "alicia": ["Rhythm"]
   }
