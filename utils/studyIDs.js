0d383b6815ea4f93eb77a27cbab3c3e19d0df16b
Pedro Neto <pasoneto@gmail.com>
2023-12-14 11:55:04 +0200

    lukilapsiStudyID

--- utils/studyIDs.js
+++ utils/studyIDs.js
@@ -1,5 +1,6 @@
 var studyIDtasks = {
     "main": ["Movement", "Singing", "Emotion", "Rhythm", "MBEMA", "SharedMeasures"],
+    "lukilapsi": ["Movement", "Singing", "EmotionAdaptive", "Rhythm", "MBEMA", "SharedMeasures"],
     "emotion":  ["Emotion", "SharedMeasures"],
     "emotionAdaptive":  ["EmotionAdaptive", "SharedMeasures"],
     "accelClass":  ["Movement"],
