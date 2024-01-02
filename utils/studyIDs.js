7feb8c26c85a19617293e986a5516b1cc5309814
Pedro Neto <pasoneto@gmail.com>
2024-01-02 11:25:26 +0200

    front page

--- utils/studyIDs.js
+++ utils/studyIDs.js
@@ -1,8 +1,8 @@
 var studyIDtasks = {
-    "main": ["Movement", "Singing", "Emotion", "Rhythm", "MBEMA", "SharedMeasures"],
+    "main": ["Movement", "Singing", "EmotionAdaptive", "EmotionAdaptive2", "Rhythm", "MBEMA", "SharedMeasures"],
     "lukilapsi": ["Movement", "Singing", "EmotionAdaptive", "Rhythm", "MBEMA", "SharedMeasures"],
     "emotion":  ["Emotion", "SharedMeasures"],
-    "emotionAdaptive":  ["EmotionAdaptive", "SharedMeasures"],
+    "emotionAdaptive":  ["EmotionAdaptive", "EmotionAdaptive2", "SharedMeasures"],
     "accelClass":  ["Movement"],
     "movementWhole":  ["Movement", "Rhythm", "SharedMeasures"],
     "alicia": ["Rhythm"]
