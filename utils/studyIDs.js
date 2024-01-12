18a05103dd47b96eb3da64292eb9ddf9425299e0
Pedro Neto <pasoneto@gmail.com>
2024-01-12 10:42:09 +0200

    old emotion version

--- utils/studyIDs.js
+++ utils/studyIDs.js
@@ -2,6 +2,7 @@ var studyIDtasks = {
     "main": ["Movement", "Singing", "EmotionAdaptive", "EmotionAdaptive2", "Rhythm", "MBEMA", "SharedMeasures"],
     "lukilapsi": ["Movement", "Singing", "EmotionAdaptive", "Rhythm", "MBEMA", "SharedMeasures"],
     "emotion":  ["Emotion", "SharedMeasures"],
+    "emotionOld": ["Movement", "Singing", "Emotion", "Rhythm", "MBEMA", "SharedMeasures"],
     "emotionAdaptive":  ["EmotionAdaptive", "EmotionAdaptive2", "SharedMeasures"],
     "accelClass":  ["Movement"],
     "movementWhole":  ["Movement", "Rhythm", "SharedMeasures"],
