f36c6f88cde848ada6a86b840014ee95ec0b24a3
Pedro Neto <pasoneto@gmail.com>
2024-01-26 17:01:30 +0200

    friends strangers

--- utils/studyIDs.js
+++ utils/studyIDs.js
@@ -1,5 +1,6 @@
 var studyIDtasks = {
     "main": ["Movement", "Singing", "EmotionAdaptive", "EmotionAdaptive2", "Rhythm", "MBEMA", "SharedMeasures"],
+    "friendsStrangers": ["Movement", "Singing", "EmotionAdaptive", "EmotionAdaptive2", "Rhythm", "MBEMA"],
     "lukilapsi": ["Movement", "Singing", "EmotionAdaptive", "EmotionAdaptive2", "Rhythm", "MBEMA", "SharedMeasures"],
     "emotion":  ["Emotion", "SharedMeasures"],
     "emotionOld": ["Movement", "Singing", "Emotion", "Rhythm", "MBEMA", "SharedMeasures"],
