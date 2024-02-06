1650527563e56032072e86003486837f538afdb6
Pedro Neto <pasoneto@gmail.com>
2024-02-06 08:39:08 +0200

    remove emotion 2 for lukilapsi

--- utils/studyIDs.js
+++ utils/studyIDs.js
@@ -1,7 +1,7 @@
 var studyIDtasks = {
     "main": ["Movement", "Singing", "EmotionAdaptive", "EmotionAdaptive2", "Rhythm", "MBEMA", "SharedMeasures"],
     "friendsStrangers": ["Movement", "Singing", "EmotionAdaptive", "EmotionAdaptive2", "Rhythm", "MBEMA"],
-    "lukilapsi": ["Movement", "Singing", "EmotionAdaptive", "EmotionAdaptive2", "Rhythm", "MBEMA", "SharedMeasures"],
+    "lukilapsi": ["Movement", "Singing", "EmotionAdaptive", "Rhythm", "MBEMA", "SharedMeasures"],
     "emotion":  ["Emotion", "SharedMeasures"],
     "emotionOld": ["Movement", "Singing", "Emotion", "Rhythm", "MBEMA", "SharedMeasures"],
     "emotionAdaptive":  ["EmotionAdaptive", "EmotionAdaptive2", "SharedMeasures"],
